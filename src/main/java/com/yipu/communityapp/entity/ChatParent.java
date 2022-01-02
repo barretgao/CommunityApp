d2d759cf3be15ac5e003136e3f8054080ff6f88c
zhangxinrong <784548253@qq.com>
2022-01-02 00:01:03 -0500

    add entity and part of the UserDao

--- src/main/java/com/yipu/communityapp/entity/ChatParent.java
+++ src/main/java/com/yipu/communityapp/entity/ChatParent.java
@@ -1,2 +1,69 @@
-package com.yipu.communityapp.entity;public class ChatParent {
+package com.yipu.communityapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "ChatParent")
+public class ChatParent implements Serializable {
+    private static final long serialVersionUID = 7551999649936522523L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int pid;
+
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "ChatParent", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<ReplyChild> replyChildList;
+
+    private String postTime;
+
+    private String postInfo;
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getPostTime() {
+        return postTime;
+    }
+
+    public void setPostTime(String postTime) {
+        this.postTime = postTime;
+    }
+
+    public String getPostInfo() {
+        return postInfo;
+    }
+
+    public void setPostInfo(String postInfo) {
+        this.postInfo = postInfo;
+    }
+
+    public List<ReplyChild> getReplyChildList() {
+        return replyChildList;
+    }
+
+    public void setReplyChildList(List<ReplyChild> replyChildList) {
+        this.replyChildList = replyChildList;
+    }
+
 }
--- src/main/java/com/yipu/communityapp/entity/ReplyChild.java
+++ src/main/java/com/yipu/communityapp/entity/ReplyChild.java
@@ -1,2 +1,69 @@
-package com.yipu.communityapp.entity;public class ReplyChild {
+package com.yipu.communityapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "ReplyChild")
+public class ReplyChild implements Serializable {
+    private static final long serialVersionUID = 2455760938054036111L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int cid;
+
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne
+    @JsonIgnore
+    private ChatParent chatParent;
+
+    private String replyTime;
+
+    private String replyInfo;
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ChatParent getChatParent() {
+        return chatParent;
+    }
+
+    public void setChatParent(ChatParent chatParent) {
+        this.chatParent = chatParent;
+    }
+
+    public String getReplyTime() {
+        return replyTime;
+    }
+
+    public void setReplyTime(String replyTime) {
+        this.replyTime = replyTime;
+    }
+
+    public String getReplyInfo() {
+        return replyInfo;
+    }
+
+    public void setReplyInfo(String replyInfo) {
+        this.replyInfo = replyInfo;
+    }
+
 }

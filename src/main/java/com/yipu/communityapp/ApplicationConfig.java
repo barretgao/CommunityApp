d53d214d510850729f3b180d2b3570edf0e9724a
barretgao <yipu.gao@vanderbilt.edu>
2022-01-03 11:19:28 -0500

    two main config file

--- src/main/java/com/yipu/communityapp/ApplicationConfig.java
+++ src/main/java/com/yipu/communityapp/ApplicationConfig.java
@@ -1,2 +1,44 @@
-package com.yipu.communityapp;public class ApplicationConfig {
+package com.yipu.communityapp;import java.util.Properties;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+//load necessary class to run spring web mvc functionality.
+@EnableWebMvc
+public class ApplicationConfig {
+    @Bean(name = "sessionFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.yipu.communityapp.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        String RDS_INSTANCE = "community-app.cywf83ckl70x.us-east-2.rds.amazonaws.com";
+        String USERNAME = "admin";
+        String PASSWORD = "1234567890";
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://" + RDS_INSTANCE + ":3306/CommunityApp?createDatabaseIfNotExist=true&serverTimezone=UTC");
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        return hibernateProperties;
+    }
 }
--- src/main/java/com/yipu/communityapp/SecurityConfig.java
+++ src/main/java/com/yipu/communityapp/SecurityConfig.java
@@ -1,2 +1,60 @@
-package com.yipu.communityapp;public class SecurityConfig {
+package com.yipu.communityapp;import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    //this method is for authorization part.
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //csrf cross site request forgery.
+                .csrf().disable()
+                //support form based authorization.
+                //form element in the http request.
+                .formLogin()
+                //if there is a failure, we forward the add the url.
+                //need a controller to handler this part of url.
+                //error is the key, true is the value.
+                .failureForwardUrl("/login?error=true");
+        http
+                //authorize the request.
+                .authorizeRequests()
+                //when user request this following patterns, we need authorization.
+                //hasAuthority will check if the user has the authroized role.
+                .antMatchers("/dashboard/*", "/chat").hasAuthority("ROLE_USER")
+                //the rest of request does not need authorization.
+                .anyRequest().permitAll();
+    }
+
+    @Override
+    //override this method to modify the authentication part.
+    //security filter can automatically grab the username and password.
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                //? is a placeholder that store user passed in information.
+                .usersByUsernameQuery("SELECT email, password, enabled FROM customer WHERE email=?")
+                .authoritiesByUsernameQuery("SELECT email, authorities FROM authorities WHERE email=?");
+
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    //this will not encode the password send from the url.
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 }
